d8038fb00c71365abdbe7d1368a3131ee235c259
peterflem <peter.t.flem@gmail.com>
2013-11-25 20:38:56 +0100

    Set up grunt to compuile .less files on live reload. Set up the grunt build task to handle index.html and .less files

--- markup/gruntfile.js
+++ markup/gruntfile.js
@@ -1,6 +1,62 @@
 module.exports = function (grunt) {
 
-	grunt.initConfig({
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
+	grunt.initConfig({
+		less : {
+			dev: {
+				files: {
+					'stylesheets/css/main.css': 'stylesheets/less/main.less'
+				}
+			},
+			build: {
+				options: {
+					cleancss: true
+				},
+				files: {
+					'build/css/main.css': 'stylesheets/less/main.less'
+				}
+			}
+		},
+
+		clean: {
+			css: ['css/main.css'],
+			build: ['build/*']
+		},
+
+		copy: {
+			build: {
+				files: [
+					{ src: 'index.html', dest: 'build/' }
+				]
+			}
+		},
+
+		delta: {
+			options: {
+				livereload: true
+			},
+
+			less: {
+				files: 'stylesheets/less/**/*.less',
+				tasks: ['clean:css', 'less:dev']
+			},
+
+			html: {
+				files: '**/*.html'
+			}
+		}
 	});
+
+	grunt.renameTask('watch', 'delta');
+
+	grunt.registerTask('watch', ['delta']);
+	grunt.registerTask('build', [
+		'clean:build',	// delete the previous build folder
+		'less:build',	// compile less into build/css/
+		'copy:build'	// copy the other needed files into the build folder (.html ...)
+	]);
 }
