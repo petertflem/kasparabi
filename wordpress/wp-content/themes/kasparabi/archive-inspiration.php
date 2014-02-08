27e89299230abbba02fa97381ccfabb8236c5c64
petfle <peter.tollnes.flem@knowit.no>
2014-02-08 12:47:19 +0100

    Added basic email submition form with some validation

--- wordpress/wp-content/themes/kasparabi/archive-inspiration.php
+++ wordpress/wp-content/themes/kasparabi/archive-inspiration.php
@@ -1,58 +1,56 @@
 <?php get_header(); ?>
-<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
-        <!-- START INSPIRATION -->
-        <div class="container references">
-            <div class="row">
-                <div class="col-xs-12">
-                    <h1><?php _e('Inspiration', 'kasparabi'); ?></h1>
-                </div>
-            </div>
-            <div class="row">
-                
-                <?php
-                    global $wp_query;
-                    $iterations = 0;
-
-                    if (have_posts()) : while (have_posts()) : the_post();
-
-                        $open = !( $iterations % 3 ) ? '<div class="row">' : '';
-                        $close = !( $iterations % 3 ) && $iterations ? '</div>' : '';
-                        echo $close.$open;
-
-                        $iterations++;
-                ?>
-
-                    <div class="col-sm-4 inspiration-gallery">
-
-                        <?php 
-                            $thumb_id = get_post_thumbnail_id();
-                            $thumb_url = wp_get_attachment_image_src($thumb_id)[0];
-                        ?>
-
-                        <a href="<?php echo $thumb_url; ?>" title="<?php echo the_title(); ?>">
-                            <?php 
-                                if (has_post_thumbnail())
-                                    the_post_thumbnail('post-thumbnail', array( 'class' => 'img-responsive' ))
-                            ?>
-                            <h4><?php the_title(); ?></h4>
-                        </a>
-                    </div>
-
-                <?php endwhile; else : ?>
-                    <p><?php _e('No inspirations found', 'kasparabi'); ?></p>
-                <?php endif; ?>
-            </div>
-            <div class="row">
-                <div class="col-xs-12">
-                    <?php 
-                        previous_posts_link(__('&larr; newer', 'kasparabi'));
-
-                        if ($wp_query->max_num_pages > $paged)
-                            next_posts_link(__('older &rarr;', 'kasparabi'));
-                    ?>
-                </div>
+    <!-- START INSPIRATION -->
+    <div class="container references">
+        <div class="row">
+            <div class="col-xs-12">
+                <h1><?php _e('Inspiration', 'kasparabi'); ?></h1>
             </div>
         </div>
-        <!-- END INSPIRATION -->     
+        <div class="row">
+            
+            <?php
+                global $wp_query;
+                $iterations = 0;
 
+                if (have_posts()) : while (have_posts()) : the_post();
+
+                    $open = !( $iterations % 3 ) ? '<div class="row">' : '';
+                    $close = !( $iterations % 3 ) && $iterations ? '</div>' : '';
+                    echo $close.$open;
+
+                    $iterations++;
+            ?>
+
+                <div class="col-sm-4 inspiration-gallery">
+
+                    <?php 
+                        $thumb_id = get_post_thumbnail_id();
+                        $thumb_url = wp_get_attachment_image_src($thumb_id, 'full');
+                    ?>
+
+                    <a href="<?php echo $thumb_url[0]; ?>" title="<?php echo the_title(); ?>">
+                        <?php 
+                            if (has_post_thumbnail())
+                                the_post_thumbnail('post-thumbnail', array( 'class' => 'img-responsive' ))
+                        ?>
+                        <h4><?php the_title(); ?></h4>
+                    </a>
+                </div>
+
+            <?php endwhile; else : ?>
+                <p><?php _e('No inspirations found', 'kasparabi'); ?></p>
+            <?php endif; ?>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <?php 
+                    previous_posts_link(__('&larr; newer', 'kasparabi'));
+
+                    if ($wp_query->max_num_pages > $paged)
+                        next_posts_link(__('older &rarr;', 'kasparabi'));
+                ?>
+            </div>
+        </div>
+    </div>
+    <!-- END INSPIRATION -->     
 <?php get_footer(); ?>
--- wordpress/wp-content/themes/kasparabi/page-contact-us.php
+++ wordpress/wp-content/themes/kasparabi/page-contact-us.php
@@ -3,6 +3,69 @@
         Template Name: Contact Us
     */
 ?>
+<?php
+ 
+    //response generation function
+    $response = "";
+
+    //function to generate response
+    function my_contact_form_generate_response($type, $message){
+
+        global $response;
+
+        if($type == "success") $response = "<div class='success'>{$message}</div>";
+        else $response = "<div class='error'>{$message}</div>";
+
+    }
+
+    //response messages
+    $not_human       = "Human verification incorrect.";
+    $missing_content = "Please supply all information.";
+    $email_invalid   = "Email Address Invalid.";
+    $message_unsent  = "Message was not sent. Try Again.";
+    $message_sent    = "Thanks! Your message has been sent.";
+     
+    //user posted variables
+    $name = $_POST['message_name'];
+    $email = $_POST['message_email'];
+    $message = $_POST['message_text'];
+    $human = $_POST['message_human'];
+     
+    //php mailer variables
+    $to = get_option('admin_email');
+    $subject = "Someone sent a message from " . get_bloginfo('name');
+    $headers = 'From: '. $email . "rn" .
+        'Reply-To: ' . $email . "rn";
+
+    if(!$human == 0){
+        if($human != 2) 
+            my_contact_form_generate_response("error", $not_human); //not human!
+        else {
+
+            //validate email
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+                my_contact_form_generate_response("error", $email_invalid);
+            else //email is valid
+            {
+                //validate presence of name and message
+                if(empty($name) || empty($message)){
+                    my_contact_form_generate_response("error", $missing_content);
+                }
+                else //ready to go!
+                {
+                    $sent = wp_mail($to, $subject, strip_tags($message), $headers);
+                    
+                    if($sent) 
+                        my_contact_form_generate_response("success", $message_sent); //message sent!
+                    else 
+                        my_contact_form_generate_response("error", $message_unsent); //message wasn't sent
+                }
+            }
+        }
+    }
+    else if ($_POST['submitted']) my_contact_form_generate_response("error", $missing_content);
+
+?>
 <?php get_header(); ?>
         
     <!-- CONTANT US -->
@@ -29,7 +92,52 @@
             <div class="row">
                 <div class="col-xs-12">
                     <h4><?php _e('Send us an email', 'kasparabi'); ?></h4>
-                    <form role="form">
+
+                    <style type="text/css">
+                        .error{
+                            padding: 5px 9px;
+                            border: 1px solid red;
+                            color: red;
+                            border-radius: 3px;
+                        }
+                     
+                        .success{
+                            padding: 5px 9px;
+                            border: 1px solid green;
+                            color: green;
+                            border-radius: 3px;
+                        }
+                     
+                        form span{
+                            color: red;
+                        }
+                    </style>
+                     
+                    <div id="respond">
+                        <?php echo $response; ?>
+                        <form role="form" action="<?php the_permalink(); ?>" method="post">
+                            <div class="form-group">
+                                <label for="name">Name: *</label>
+                                <input type="text" name="message_name" class="form-control" value="<?php echo esc_attr($_POST['message_name']); ?>" />
+                            </div>
+                            <div class="form-group">
+                                <label for="message_email">Email: *</label>
+                                <input type="text" name="message_email" class="form-control" value="<?php echo esc_attr($_POST['message_email']); ?>" />
+                            </div>
+                            <div class="form-group">
+                                <label for="message_text">Message: *</label>
+                                <textarea type="text" rows="4" class="form-control" name="message_text"><?php echo esc_textarea($_POST['message_text']); ?></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="message_human">Human Verification: *<input type="text" style="width: 60px;" class="form-control" name="message_human"> + 3 = 5</label>
+                            </div>
+
+                            <input type="hidden" name="submitted" value="1" />
+                            <button type="submit" class="btn btn-default">Send epost</button>
+                        </form>
+                    </div>
+
+                    <!--form role="form">
                         <div class="form-group">
                             <label for="email-subject">Emne</label>
                             <input type="email" class="form-control" id="email-subject" placeholder="Emne..." />
@@ -39,7 +147,8 @@
                             <textarea class="form-control" id="email-content" placeholder="Innhold..." rows="4" ></textarea>
                         </div>
                         <button type="submit" class="btn btn-default">Send epost</button>
-                    </form>
+                    </form-->
+
                 </div>
             </div>
         </div>
