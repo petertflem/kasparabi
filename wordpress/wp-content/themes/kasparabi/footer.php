c75c55641b3296df321242dfa0bbe88e5adbde70
petfle <peter.tollnes.flem@knowit.no>
2014-02-20 18:12:38 +0100

    Fixed the footer so it works on small screens

--- wordpress/wp-content/themes/kasparabi/footer.php
+++ wordpress/wp-content/themes/kasparabi/footer.php
@@ -4,14 +4,23 @@
         <footer>
             <div class="container">
                 <div class="row">
-                    <div class="col-xs-5">
+                    <!--<div class="col-xs-5">
                         <div class="copyright">
                             <a href="<?php echo home_url(); ?>">copyright kaspara</a>
                         </div>
                     </div>
                     <div class="col-xs-7 social-media">
-                        <div class="facebook ceter-vertically"></div>
-                        <div class="instagram ceter-vertically"></div>
+                        <div class="facebook"></div>
+                        <div class="instagram"></div>
+                    </div>-->
+                    <div class="col-xs-12">
+                        <div class="copyright">
+                            <a href="<?php echo home_url(); ?>">copyright kaspara</a>
+                        </div>
+                        <div class="social-media">
+                            <div class="facebook"></div>
+                            <div class="instagram"></div>
+                        </div>
                     </div>
                 </div>
             </div>
