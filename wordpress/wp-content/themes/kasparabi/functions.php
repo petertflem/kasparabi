449572865d1f226e5b206d511aa534a88f3d1795
petfle <peter.tollnes.flem@knowit.no>
2014-11-22 16:28:41 +0100

    We now show where you are in the menu by adding a gray background color to the active element

--- wordpress/wp-content/themes/kasparabi/functions.php
+++ wordpress/wp-content/themes/kasparabi/functions.php
@@ -26,6 +26,21 @@
 
 
 	/*--------------------------------------------------------------------------*
+	 * Set active class on currently active menu item
+	/*--------------------------------------------------------------------------*/
+	add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+	function special_nav_class($classes, $item){
+			 if( in_array('current-menu-item', $classes) ){
+							 $classes[] = 'active ';
+			 }
+			 return $classes;
+	}
+
+
+
+
+
+	/*--------------------------------------------------------------------------*
 	 * Add theme support
 	/*--------------------------------------------------------------------------*/
 	add_theme_support( 'post-thumbnails' );
